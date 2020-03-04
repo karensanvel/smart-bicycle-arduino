a0f4810a6edd9e94253bf3b6ab421881aadf7ddc
Ilmar López <ilmarlopezr@hotmail.com>
2020-03-03 19:50:53 -0800

    Create component example

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ExampleComponent',
+  data() {
+    return {
+      title: 'Hola Karen',
+    };
+  },
+};
+</script>
+<style lang="css" scope></style>
