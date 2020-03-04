7ce0427289df74f8b6f8e36ddc2e1949208ff061
Ilmar López <ilmarlopezr@hotmail.com>
2020-03-03 19:40:32 -0800

    Update index.blade.php implementación de vuejs

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -10,19 +10,21 @@
     @yield('css')
 </head>
 <body>
-    <nav class="navBar">
-        <div id="logo"><img src="/images/logo3.png" alt="logo"></div>
-        <nav id="nav">
-            <ul>
-                <li><a href="javascript:alert('Main')">Main Dshboard</a></li>
-                <li><a href="http://facebook">Weather</a></li>
-                <li><a href="javascript:alert('Something else')">Something else</a></li>
-            </ul>
-        </nav>
-    </nav>   
-    <div id="header">Bicycle route information Dashboard</div>
-    @yield('content')
-    
+    <div id="app" class="content"><!--La equita id debe ser app, como hemos visto en app.js-->
+        <nav class="navBar">
+            <div id="logo"><img src="/images/logo3.png" alt="logo"></div>
+            <nav id="nav">
+                <ul>
+                    <li><a href="javascript:alert('Main')">Main Dshboard</a></li>
+                    <li><a href="http://facebook">Weather</a></li>
+                    <li><a href="javascript:alert('Something else')">Something else</a></li>
+                </ul>
+            </nav>
+        </nav>   
+        <div id="header">Bicycle route information Dashboard</div>
+        @yield('content')
+    </div>
     @yield('js')
+    <script src="{{asset('js/app.js')}}"></script> <!--Añadimos el js generado con webpack, donde se encuentra nuestro componente vuejs-->
 </body>
 </html>
