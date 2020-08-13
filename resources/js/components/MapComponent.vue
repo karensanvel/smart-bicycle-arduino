4a058151e8c8e52d0f6112de920a45c0878c17f8
Ilmar López <ilmarlopezr@hotmail.com>
2020-08-12 23:30:04 -0700

    Muestra un icono de panico dentro del mapa cuando el boton de panico es activado.

--- resources/js/components/MapComponent.vue
+++ resources/js/components/MapComponent.vue
@@ -9,6 +9,7 @@
 </template>
 <script>
 import L from "leaflet";
+import axios from 'axios';
 
 export default {
     name: "MapComponent",
@@ -16,6 +17,7 @@ export default {
         return {
             map: null,
             tileLayer: null,
+            marker: null,
             layer: {
                 id: 0,
                 name: "Route traveled",
@@ -27,50 +29,27 @@ export default {
     mounted() {
         this.initMap();
         this.initLayer();
+
+        setTimeout(() => {
+            this.activateAlarm();
+        }, 1000)
     },
     methods: {
         initLayer() {
-            var p = new L.LatLng(44.4294834, 26.1002004);
-            this.layer.pointList.push(p);
-            var p = new L.LatLng(44.4296068, 26.0988593);
-            this.layer.pointList.push(p);
+            var self = this;
 
-            var routepolyline = new L.Polyline(this.layer.pointList, {
-                color: "blue",
-                weight: 5,
-                opacity: 0.5,
-                smoothFactor: 1
+            var markerIcon = L.icon({
+                iconUrl: "images/marker.png",
+                iconSize: [32, 32]
+            });
+            this.marker = L.marker([32.533, -117.05], {
+                icon: markerIcon
+            }).on('click', function (e) {
+                self.map.setView(e.target.getLatLng(), 15)
             }).addTo(this.map);
-
-            if (this.layer.pointList.length > 0) {
-                this.map.setView(this.layer.pointList[0], 11, {
-                    animation: true
-                });
-            }
-
-            // var sec = 0;
-            // var bicycleIcon = L.icon({
-            //     iconUrl:
-            //         "https://image.flaticon.com/icons/png/512/1183/1183088.png",
-            //     iconSize: [32, 32]
-            // });
-            // var bicycle = L.marker([44.4294834, 26.1002004], {
-            //     icon: bicycleIcon
-            // }).addTo(this.map);
-
-            // var self = this;
-            // var timer = setInterval(function() {
-            //     if (sec < self.layer.pointList.length - 1) {
-            //         sec++;
-            //         self.map.setView(self.layer.pointList[sec], 11, {
-            //             animation: true
-            //         });
-            //         bicycle.setLatLng(self.layer.pointList[sec]);
-            //     }
-            // }, 300);
         },
         initMap() {
-            this.map = L.map("map").setView([38.63, -90.23], 12);
+            this.map = L.map("map").setView([32.533, -117.05], 12);
             this.tileLayer = L.tileLayer(
                 "https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png",
                 {
@@ -80,6 +59,37 @@ export default {
                 }
             );
             this.tileLayer.addTo(this.map);
+        },
+        activateAlarm() {
+            axios.get('api/getLastData/').then((res => {
+                var self = this;
+                if (res.data.datos[0].alarm === "1") {
+                    this.map.removeLayer(this.marker);
+        
+                    var markerIcon = L.icon({
+                        iconUrl: "images/sos.png",
+                        iconSize: [60, 60],
+                        className: 'blink'
+                    });
+                    this.marker = L.marker([32.533, -117.05], {
+                        icon: markerIcon
+                    }).on('click', function (e) {
+                        self.map.setView(e.target.getLatLng(), 16)
+                    }).addTo(this.map).bindPopup(
+                        'El usuario ha activado la alarma.'
+                    ).openPopup();
+                } else {
+                    var markerIcon = L.icon({
+                        iconUrl: "images/marker.png",
+                        iconSize: [32, 32]
+                    });
+                    this.marker = L.marker([32.533, -117.05], {
+                        icon: markerIcon
+                    }).on('click', function (e) {
+                        self.map.setView(e.target.getLatLng(), 16)
+                    }).addTo(this.map);
+                }
+            }))
         }
     }
 };
@@ -87,6 +97,17 @@ export default {
 <style lang="css" scope>
 .map {
     height: 450px;
+}
+.leaflet-popup-content {
+    color: red;
+}
+.blink {
+    animation: fade 1s infinite alternate;
+}
+@keyframes fade { 
+    from { 
+        opacity: 0.3;
+    }
 }
 /* https://travishorn.com/interactive-maps-with-vue-leaflet-5430527353c8 */
 </style>
